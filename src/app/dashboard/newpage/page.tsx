ba7479212f83bc50b2294701ba1ae9528e31510a
Athos <athospugliesedev@gmail.com>
2024-03-29 17:56:46 -0300

    verify role

--- src/app/dashboard/newpage/page.tsx
+++ src/app/dashboard/newpage/page.tsx
@@ -45,8 +45,6 @@ import {
   TableRow,
 } from "@/components/ui/table"
 
-export const description =
-  "An application shell with a header and main content area. The header has a navbar, a search input and and a user nav dropdown. The user nav is toggled by a button with an avatar image."
 
 export const iframeHeight = "825px"
 
