13a0191a5f4ca1922916102cbf73af47ae7eaaa3
Athos <athospugliesedev@gmail.com>
2024-04-19 21:15:26 -0300

    reset page from email

--- src/app/reset-password/[slug]/page.tsx
+++ src/app/reset-password/[slug]/page.tsx
@@ -2,6 +2,8 @@
 import { useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 import axios from 'axios';
+import logo from '../../assets/logo1.png'
+import Image from 'next/image';
 
 export default function Page({ params }: { params: { slug: string } }) {
   const [newPassword, setNewPassword] = useState('');
@@ -28,16 +30,31 @@ export default function Page({ params }: { params: { slug: string } }) {
   };
 
   return (
-    <div>
-      <h1>Redefinir Senha</h1>
-      <p>Token de redefinição de senha: {params.slug}</p>
-      <p>Insira sua nova senha:</p>
-      <input
-        type="password"
-        value={newPassword}
-        onChange={(e) => setNewPassword(e.target.value)}
-      />
-      <button onClick={handleResetPassword}>Redefinir Senha</button>
-    </div>
+    <main className='bg-gray-900 h-screen justify-center center p-16'>
+      <div className="max-w-md mx-auto p-6 bg-gray-100 rounded-lg shadow-xl flex flex-col justify-center">
+      <Image
+            src={logo}
+            alt="Logo Unicap Sistema de Avaliação"
+            loading="lazy"	
+            className='p-6'
+          />
+        <h1 className="text-2xl font-bold mb-4">Redefinir Senha</h1>
+        <p className="mt-4">Insira sua nova senha:</p>
+        <input
+          className="border border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:ring focus:white"
+          type="password"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
+        />
+        <button
+          className="bg-default text-white rounded-md px-4 py-2 mt-4 hover:bg-default focus:outline-none focus:ring focus:bg-default"
+          onClick={handleResetPassword}
+        >
+          Redefinir Senha
+        </button>
+
+      </div>
+    </main>
+
   );
 }
