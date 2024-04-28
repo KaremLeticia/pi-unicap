17bf9a283b4b3481d120c4f222dcaecb4000f727
Athos <athospugliesedev@gmail.com>
2024-04-28 16:44:30 -0300

    changes in ui login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,6 +28,7 @@ export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false); // Estado para controlar o carregamento
+  const [loadingtwo, setLoadingtwo] = useState(false); // Estado para controlar o carregamento
   const [error, setError] = useState<string | undefined>(undefined);
   const { setRole } = useRole();
 
@@ -63,6 +64,7 @@ export default function Login() {
 
   const handlePassword = async () => {
     router.push('/forget-password');
+    setLoadingtwo(true)
   }
 
 
@@ -80,7 +82,7 @@ export default function Login() {
             <h1 className="text-2xl font-semibold">Novo aluno?</h1>
             <p className=''>Crie uma conta com e-mail</p>
             <button onClick={handleRegister} className="bg-default w-full h-10 rounded self-center hover:bg-default/90 text-white relative">
-              {loading ? <LoaderCircle className="absolute inset-0 m-auto animate-spin" /> : "Criar conta"}
+              {loadingtwo ? <LoaderCircle className="absolute inset-0 m-auto animate-spin" /> : "Criar conta"}
             </button>
               <div className="relative">
                 <div className="absolute inset-0 flex items-center">
